6e030fb48db8f332dc382528cd0cb6aa970791c6
dgageot <david@gageot.net>
2013-06-09 17:44:20 +0200

    More apps

--- osx/homebrew.sh
+++ osx/homebrew.sh
@@ -2,3 +2,9 @@ brew update
 
 brew tap phinze/homebrew-cask
 brew install brew-cask
+brew cask install super-duper
+brew cask install transmission
+brew cask install imageoptim
+brew cask install imagealpha
+brew cask install namemangler
+brew cask install open-office
