eae45b98a62934c01da858be2fe0c85e8ab95bb5
David Gageot <david@gageot.net>
2013-10-07 20:42:58 +0200

    Fix script

--- osx/homebrew.sh
+++ osx/homebrew.sh
@@ -12,68 +12,51 @@ brew upgrade
 
 if [[ $(brew tap) != *cask* ]]; then
 	brew tap phinze/homebrew-cask
-	brew cask alfred link
+	brew install brew-cask
 fi
 
-FORMULAS_TO_INSTALL=(
-	ack
-	bash-completion
-	brew-cask
-	cloudbees-sdk
-	dos2unix
-	git
-	gradle
-	heroku-toolbelt
-	hub
-	ioke
-	jq
-	jsonpp
-	maven
-	md5sha1sum
-	mongodb
-	node
-	parallel
-	phantomjs
-	pigz
-	pngcrush
-	sonar
-	ssh-copy-id
-	the_silver_searcher
-	tig
-	tree
-	watch
-	wget
-)
+brew install ack
+brew install bash-completion
+brew install cloudbees-sdk
+brew install dos2unix
+brew install gradle
+brew install heroku-toolbelt
+brew install hub
+brew install ioke
+brew install jq
+brew install jsonpp
+brew install maven
+brew install md5sha1sum
+brew install mongodb
+brew install node
+brew install parallel
+brew install phantomjs
+brew install pigz
+brew install pngcrush
+brew install sonar
+brew install ssh-copy-id
+brew install the_silver_searcher
+brew install tig
+brew install tree
+brew install watch
+brew install wget
 
-for i in $(brew list); do
-	FORMULAS_TO_INSTALL=(${FORMULAS_TO_INSTALL[@]//*$i*})
-done
-
-for FORMULA in ${FORMULAS_TO_INSTALL}; do
-	brew install ${FORMULA}
-done
-
-CASK_FORMULAS_TO_INSTALL=(
-	github
-	gitx
-	growlnotify
-	imagealpha
-	imageoptim
-	namemangler
-	open-office
-	pacifist
-	slate
-	subtitles
-	super-duper
-	transmission
-	vagrant
-	virtualbox
-)
-
-for i in $(brew cask list); do
-	CASK_FORMULAS_TO_INSTALL=(${CASK_FORMULAS_TO_INSTALL[@]//*$i*})
-done
-
-for FORMULA in ${CASK_FORMULAS_TO_INSTALL}; do
-	brew cask install ${CASK_FORMULAS_TO_INSTALL}
-done
+brew cask install alfred
+brew cask install google-chrome
+brew cask install github
+brew cask install gitx
+brew cask install growlnotify
+brew cask install imagealpha
+brew cask install imageoptim
+brew cask install intellij-ultimate
+brew cask install iterm2
+brew cask install namemangler
+brew cask install open-office
+brew cask install pacifist
+brew cask install slate
+brew cask install subtitles
+brew cask install super-duper
+brew cask install textmate
+brew cask install transmission
+brew cask install vagrant
+brew cask install virtualbox
