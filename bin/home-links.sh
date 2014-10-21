436e4aa31f74e71338f638303c05e81806e6946b
David Gageot <david@gageot.net>
2014-10-21 09:05:23 +0200

    Unused

--- bin/home-links.sh
+++ bin/home-links.sh
@@ -4,4 +4,3 @@ sudo rm -Rf ~/Documents ~/java ~/Movies ~/Pictures
 sudo ln -s "${HOME}/Google Drive/Documents" ${HOME}/Documents
 sudo ln -s "${HOME}/Google Drive/Movies" ${HOME}/Movies
 sudo ln -s "${HOME}/Google Drive/Pictures" ${HOME}/Pictures
-sudo ln -s "${HOME}/Google Drive/Documents/java" ${HOME}/java
