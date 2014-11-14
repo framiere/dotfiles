bd545b413d8689daabf70d8a185c0aa5bcf33a02
David Gageot <david@gageot.net>
2014-11-14 08:59:07 +0100

    FIX script

--- bin/home-links.sh
+++ bin/home-links.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo rm -Rf ~/Documents ~/java ~/Movies ~/Pictures
+sudo rm -Rf ~/Documents ~/Movies ~/Pictures
 sudo ln -s "${HOME}/Google Drive/Documents" ${HOME}/Documents
 sudo ln -s "${HOME}/Google Drive/Movies" ${HOME}/Movies
 sudo ln -s "${HOME}/Google Drive/Pictures" ${HOME}/Pictures
