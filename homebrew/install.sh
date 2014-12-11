1f6f0be75758312f761205004be783048fc64a4e
Florent Ramière <florent@ramiere.com>
2014-12-11 09:37:22 +0100

    no golo

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -51,7 +51,6 @@ fi
 eval "$(jenv init -)"
 jenv enable-plugin export
 jenv enable-plugin ant
-jenv enable-plugin golo
 jenv enable-plugin gradle
 jenv enable-plugin groovy
 jenv enable-plugin maven
